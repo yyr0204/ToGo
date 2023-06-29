d7b4c844a6478a22af58756f56c80b2e5d6eec2d
yyr0204 <yyr0204@naver.com>
2023-06-29 15:25:23 +0900

    file 지훈

--- ToGo/src/main/java/test/spring/controller/park/FestivalController.java
+++ ToGo/src/main/java/test/spring/controller/park/FestivalController.java
@@ -1,5 +1,10 @@
 package test.spring.controller.park;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/main/*")
 public class FestivalController {
 
 }
