60749a3e8aeb8bd7fa74ec7c9e614dabcd7910a1
YoonSangGu <ysg0715@gmail.com>
2023-06-29 15:37:42 +0900

    Halo

---
+++ ToGo/src/main/java/test/spring/controller/map/MapController.java
@@ -0,0 +1,9 @@
+package test.spring.controller.map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/map/*")
+@Controller
+public class MapController {
+}
