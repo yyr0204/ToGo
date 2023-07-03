c5b48f48ac9934e9368728ffdc22b25b79bb13e4
YoonSangGu <ysg0715@gmail.com>
2023-07-03 16:35:55 +0900

    Halo

--- ToGo/src/main/java/test/spring/controller/map/MapController.java
+++ ToGo/src/main/java/test/spring/controller/map/MapController.java
@@ -1,13 +1,19 @@
 package test.spring.controller.map;
-
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-@RequestMapping("/map/*")
 @Controller
+@RequestMapping("/map/*")
 public class MapController {
     @RequestMapping("testMap")
     public String testMap(){
-        return "testMap";
+        return "/map/testMap";
     }
+    @RequestMapping("/list")
+    public String list(Model model){
+
+        model.addAttribute("list","ASd");
+        return "/map/list";
+    }
+
 }
