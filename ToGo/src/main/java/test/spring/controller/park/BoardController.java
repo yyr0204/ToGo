7ff6536cd3df22ea9b6ce71ecbf6501d396db5c6
yyr0204 <yyr0204@naver.com>
2023-07-21 13:42:04 +0900

    board controller 수정

--- ToGo/src/main/java/test/spring/controller/park/BoardController.java
+++ ToGo/src/main/java/test/spring/controller/park/BoardController.java
@@ -9,24 +9,25 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import test.spring.component.park.BeachResultData;
+import test.spring.component.park.CmBoardDTO;
 import test.spring.component.park.FaqBoardDTO;
 import test.spring.component.park.FstvlDTO;
+import test.spring.component.park.PageResolver;
 import test.spring.component.park.QnaDTO;
-import test.spring.component.park.QnaPage;
 import test.spring.service.park.FaqService;
 import test.spring.service.park.FestivalService;
 import test.spring.service.park.QnaService;
@@ -40,8 +41,6 @@ public class BoardController {
 	private QnaService qnaservice;
 	@Autowired 
 	private FestivalService festivalService;
-	@Autowired 
-	private QnaPage page;
 	
 	//FAQ
 	@RequestMapping("/faqWriteForm")
@@ -49,11 +48,12 @@ public class BoardController {
 		return "/park/faq/faqWriteForm";
 	}
 	@RequestMapping("/faqWritePro")
-	public String insert(FaqBoardDTO dto, Model model) {
-		int isTrue = faqService.insert(dto);
-		faqService.insert(dto);
-		model.addAttribute("isTrue",isTrue);
-		return "/park/faq/faqList";
+	public String insert(FaqBoardDTO dto, Model model,HttpSession session) {
+		// ���߿� �����ڷ� �ٲ����
+		String memId = (String) session.getAttribute("memId");
+		dto.setFaq_writer(memId);
+		model.addAttribute("success",faqService.insert(dto));
+		return "redirect:/board/faqList";
 	}
 	@RequestMapping("/faqList")
 	public String faqList(FaqBoardDTO dto, Model model) {
@@ -67,46 +67,114 @@ public class BoardController {
 		return "/park/qna/qnaWriteForm";
 	}
 	@RequestMapping("/qnaInsert")
-	public String qnaInsert(QnaDTO dto) {
+	public String qnaInsert(QnaDTO dto,HttpSession session) {
+		String memId = (String) session.getAttribute("memId");
+		dto.setWriter(memId);
 		qnaservice.qnaInsert(dto);
-		return "/park/qna/qnaWriteForm";
+		return "redirect:/board/qnaList";
 	}
 	@RequestMapping("/qnaList")
-	public String list(Model model, HttpSession session, @RequestParam(defaultValue = "1") int curPage, String search, String keyword) {
-//		//QNA Ŭ�� �ϸ� admin���� �ڵ� �α���
-//		HashMap<String, String> map = new HashMap<String, String>();
-//		//HashMap : �����͸� ���� �ڷ� ����
-//		map.put("id", "admin");
-//		map.put("pw", "1234");
-//		session.setAttribute("login_info", member.member_login(map));
-//		session.setAttribute("category", "qna");
-		
-		//DB���� �� ��� ��ȸ�ؿ� ȭ�鿡 ���
-		page.setCurPage(curPage);
-		page.setSearch(search);
-		page.setKeyword(keyword);
-		model.addAttribute("page", qnaservice.qnaList(page));
+	public String list(Model model, HttpSession session, @RequestParam(value = "pageNum",defaultValue = "1") String pageNum, String option, String keyword
+			,QnaDTO dto,@RequestParam(value = "memId", required = false) String memId) {
+		memId = (String) session.getAttribute("memId");
+		// �˻�����
+		if (keyword != null) {
+			dto.setOption(option);
+			dto.setKeyword(keyword);
+		}
+		// ���������̼�
+		int pageSize = 10; // ������ �� �Խñ� ����
+		int page = 1;
+		try {
+			if (pageNum != null && !pageNum.equals("")) {
+				page = Integer.parseInt(pageNum);
+			}
+		} catch (NumberFormatException e) {
+			e.printStackTrace();
+		}
+		// ����Ʈ �� ����
+		int total = qnaservice.totalList(dto);
+		// ù �� ��ȣ
+		int beginPage = (page - 1) * pageSize + 1;
+		// ������ �� ��ȣ
+		int endPage = beginPage + pageSize - 1;
+
+		dto.setBeginPage(beginPage);
+		dto.setEndPage(endPage);
+
+		List<QnaDTO> boardList = qnaservice.qnaList(dto);
+		PageResolver pr = new PageResolver(page, pageSize, total);
+
+		model.addAttribute("boardList", boardList);
+		model.addAttribute("pr", pr);
+		model.addAttribute("memId", memId);
+		model.addAttribute("option", option);
+		model.addAttribute("keyword", keyword);
+
 		return "/park/qna/qnaList";
 	}
 	//QNA �� �� ȭ�� ��û
-		@RequestMapping("/qnaDetail")
-		public String detail(int num, Model model) {
-			//������ QNA �ۿ� ���� ��ȸ�� ���� ó��
-			qnaservice.qnaDetail(num);
-			
-			//������ QNA �� ������ DB�� ��ȸ�ؿ� �� ȭ�鿡 ���
-			model.addAttribute("dto", qnaservice.qnaDetail(num));
-			model.addAttribute("crlf", "\r\n");
-			model.addAttribute("page", page);
-			
-			return "/park/qna/qnaDetail";
-		} 
-
+	@RequestMapping("/qnaDetail")
+	public String detail(int num, Model model) {
+		//������ QNA �ۿ� ���� ��ȸ�� ���� ó��
+		qnaservice.qnaRead(num);
+		//������ QNA �� ������ DB�� ��ȸ�ؿ� �� ȭ�鿡 ���
+		model.addAttribute("dto", qnaservice.qnaDetail(num));
+		
+		return "/park/qna/qnaDetail";
+	} 
+	//QNA �� ���� ȭ�� ��û
+	@RequestMapping("/qnaModifyForm")
+	public String modify(int num, Model model) {
+		//������ QNA �� ������ DB���� ��ȸ�ؿ� ���� ȭ�鿡 ���
+		model.addAttribute("dto", qnaservice.qnaDetail(num));
+		return "/park/qna/qnaModify";
+	} //modify()
+	
+	//QNA �� ���� ó�� ��û
+	@RequestMapping("/qnaModifyPro")
+	public String update(QnaDTO dto, HttpSession session, String title,String content,@RequestParam(value = "num") int num) {
+		//ȭ�鿡�� ������ ������ DB�� ������ �� �� ȭ������ ����
+		dto.setNum(num);
+		dto.setTitle(title);
+		dto.setContent(content);
+		qnaservice.qnaUpdate(dto);
+		return "redirect:/board/qnaDetail?num=" + dto.getNum();
+	} //update()	
+	//QNA �� ���� ó�� ��û
+	@RequestMapping("/qnaDelete")
+	public String delete(int num) {
+		//������ QNA ���� DB���� ������ �� ��� ȭ������ ����
+		qnaservice.qnaDelete(num);
+		
+		return "redirect:/board/qnaList";
+	}
+	//��� ���� ȭ�� ��û==================================================================
+	@RequestMapping("/qnaReplyForm")
+	public String reply(Model model, int num) {
+		//������ ������ ��� ���� ȭ�鿡�� �� �� �ֵ��� �Ѵ�.
+		model.addAttribute("dto", qnaservice.qnaDetail(num));
+		
+		return "park/qna/qnaReply";
+	} //reply()
+	
+	//�ű� ��� ���� ó�� ��û==============================================================
+	@RequestMapping("/qnaReplyPro")
+	public String reply_insert(QnaDTO dto, HttpSession session) {
+		//�ۼ��ڴ� ������ �ΰ��
+//		dto.setWriter(((MemberVO) session.getAttribute("login_info")).getId());
+		//�ӽ�
+		String memId = (String) session.getAttribute("memId");
+		dto.setWriter(memId);
+		//ȭ�鿡�� �Է��� ������ DB�� ������ �� ��� ȭ������ ����
+		qnaservice.qnaReplyInsert(dto);
+		return "redirect:/board/qnaList";
+	}
+	
 	//���� ���� �����̵�
 	@RequestMapping("/fstvl")
 	public String fstvl(FstvlDTO dto, Model model) {
-	    List<FstvlDTO> fstvlList = festivalService.fstvlList(dto);
-
+		List<FstvlDTO> fstvlList = festivalService.fstvl(dto);
 	    List<FstvlDTO> randomFstvlList = new ArrayList<>();
 	    if (fstvlList.size() > 5) {
 	        List<Integer> indexes = new ArrayList<>();
@@ -114,6 +182,7 @@ public class BoardController {
 	            indexes.add(i);
 	        }
 	        Collections.shuffle(indexes); // �ε����� �����ϰ� ����
+
 	        for (int i = 0; i < 5; i++) {
 	            randomFstvlList.add(fstvlList.get(indexes.get(i)));
 	        }
@@ -125,12 +194,41 @@ public class BoardController {
 	}
 	//���� ����Ʈ
 	@RequestMapping("/fstvlList")
-	public String fstvlList(FstvlDTO dto, Model model) {
-	    List<FstvlDTO> fstvlList = festivalService.fstvlList(dto);
-	    model.addAttribute("fstvlList", fstvlList);
-	    return "/park/festival/fstvlList";
-	}
+	public String fstvlList(@RequestParam(value = "pageNum", defaultValue = "1") String pageNum, Model model,FstvlDTO dto, String option, String keyword) {
+		// �˻�����
+		if (keyword != null) {
+			dto.setOption(option);
+			dto.setKeyword(keyword);
+		}
+		// ���������̼�
+		int pageSize = 5; // ������ �� �Խñ� ����
+		int page = 1;
+		try {
+			if (pageNum != null && !pageNum.equals("")) {
+				page = Integer.parseInt(pageNum);
+			}
+		} catch (NumberFormatException e) {
+			e.printStackTrace();
+		}
+		// ����Ʈ �� ����
+		int total = festivalService.selectFstvlCount(dto);
+		// ù �� ��ȣ
+		int beginPage = (page - 1) * pageSize + 1;
+		// ������ �� ��ȣ
+		int endPage = beginPage + pageSize - 1;
+		dto.setBeginPage(beginPage);
+		dto.setEndPage(endPage);
 
+		List<FstvlDTO> fstvlList = festivalService.fstvlList(dto);
+		PageResolver pr = new PageResolver(page, pageSize, total);
+
+		model.addAttribute("fstvlList", fstvlList);
+		model.addAttribute("pr", pr);
+		model.addAttribute("option", option);
+		model.addAttribute("keyword", keyword);
+
+		return "/park/festival/fstvlList";
+	}
 	//�ؼ����� api
 	@GetMapping("/beach")
 	public String getBeachInformation( Model model) {
@@ -177,17 +275,5 @@ public class BoardController {
 	    
 	    return "/park/beach";
 	}
-
-//		@GetMapping("/scrape-and-save")
-//		public String scrapeAndSaveFestivals() {
-//		    String testURL = "https://www.mcst.go.kr/kor/s_culture/festival/festivalList.jsp?pMenuCD=&pCurrentPage=%d&pSearchType=&pSearchWord=&pSeq=&pSido=&pOrder=&pPeriod=&fromDt=&toDt=";
-//
-//		    // ũ�Ѹ� �� �����ͺ��̽� ������Ʈ
-//		    for (int i = 1; i <= 12; i++) {
-//		        String url = String.format(testURL, i);
-//		        festivalService.testCrawling(url);
-//		    }
-//
-//		    return "/park/fstvl";
-//		}
+	
 }
