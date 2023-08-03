04dcb9a0e3ce25edfdc21a4ec90131aca110b44c
JeHyeon <wpgus3943@naver.com>
2023-08-03 11:04:38 +0900

    song(2023-08-03)

--- ToGo/src/main/java/test/spring/controller/choi/LoginController.java
+++ ToGo/src/main/java/test/spring/controller/choi/LoginController.java
@@ -27,20 +27,61 @@ public class LoginController {
 	
 	@RequestMapping("login")
 	public @ResponseBody String login(KakaoDTO dto,String email, HttpSession session, Model model) {
-		int count = ls.check(dto.getId());
-
-		KakaoDTO dto2 = mpservice.user_info(email);
-		
-		System.out.println(dto.getEmail());
-		session.setAttribute("memId", dto.getEmail());
-		if(count == 0) {
-			ls.kakaoInsert(dto);
-			return "question";
-		}else if((dto2.getStatus()!=null) && (dto2.getStatus().equals("N"))) {
-			return "black";
+		KakaoDTO user = mpservice.user_info2(dto.getId());
+		System.out.println(dto);
+		if(user != null) {
+			String userEmail = user.getEmail();
+			System.out.println(userEmail);
+			if(userEmail.equals("sample")) {
+				if(session.getAttribute("key")==null) {
+					session.setAttribute("key", dto.getId());
+				}
+				return "notUser";
+			}else {
+				if((user.getStatus()!=null) && (user.getStatus().equals("N"))) {
+					return "black";
+				}else {
+					return "main";
+				}
+			}
 		}else {
-			return "main";
+			if(dto.getEmail() == null) {
+				dto.setEmail("sample");
+				ls.kakaoInsert(dto);
+				session.setAttribute("key", dto.getId());
+				return "notUser";
+			}else {
+				System.out.println(dto.getEmail());
+				session.setAttribute("memId", dto.getEmail());
+				ls.kakaoInsert(dto);
+				return "question";
+			}
 		}
+	}
+	
+	@RequestMapping("email")
+	public String email(HttpSession session, Model model) {
+		String id = (String)session.getAttribute("key");
+		model.addAttribute("id", id);
+		session.invalidate();
+		return "/song/email";
+	}
+	
+	@RequestMapping("emailPro")
+	public String emailPro(String id, String email, HttpSession session, Model model) {
+		int count = ls.check2(email);
+		System.out.println(id);
+		
+		if(count == 0) {
+			ls.addEmail(id, email);
+			session.setAttribute("memId", email);
+			return "redirect:/pwSetting";
+		}else {
+			model.addAttribute("id", id);
+			model.addAttribute("re", 1);
+			return "/song/email";
+		}
+
 	}
 	
 	@RequestMapping("loginMain")
--- ToGo/src/main/java/test/spring/service/park/MyPageService.java
+++ ToGo/src/main/java/test/spring/service/park/MyPageService.java
@@ -6,6 +6,7 @@ import test.spring.component.choi.KakaoDTO;
 
 public interface MyPageService {
 	public KakaoDTO user_info(String email);
+	public KakaoDTO user_info2(String id);
 	public int update_info(KakaoDTO dto);
 	public List<KakaoDTO> userList(KakaoDTO dto);
 	public int userCount(KakaoDTO dto);
