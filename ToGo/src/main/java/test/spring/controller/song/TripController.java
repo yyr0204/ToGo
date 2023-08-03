7781ebfa13d57e2ae6809e93369022774e3f4072
JeHyeon <wpgus3943@naver.com>
2023-08-03 11:16:53 +0900

    song

--- ToGo/src/main/java/test/spring/controller/song/TripController.java
+++ ToGo/src/main/java/test/spring/controller/song/TripController.java
@@ -126,7 +126,7 @@ public class TripController {
 
                 long startTime3 = System.currentTimeMillis();
                 List<List<SampleListDTO>> daySub = dao.generateDaySubList(table, userAtmosphere, optimizedMain);
-                if (daySub.size() == 0) {
+                if (daySub == null) {
                     count++;
                     continue Loop;
                 }
