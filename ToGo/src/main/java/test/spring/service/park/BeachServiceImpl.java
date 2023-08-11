5efcff9805dfeafbb011c9709d54d8e1b6b51f64
yyr0204 <yyr0204@naver.com>
2023-08-11 17:20:50 +0900

    [update] 관리자 로그인 페이지

--- ToGo/src/main/java/test/spring/service/park/BeachServiceImpl.java
+++ ToGo/src/main/java/test/spring/service/park/BeachServiceImpl.java
@@ -13,23 +13,29 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import test.spring.component.park.BeachResultData;
 
 @Service("beachService")
-public class BeachServiceImpl implements BeachService{
-	public BeachResultData getBeachInformation() {
+public class BeachServiceImpl implements BeachService {
+
+    // 해변 정보를 가져오는 메서드
+    public BeachResultData getBeachInformation() {
         try {
+            // API 요청을 위한 URL 생성
             StringBuilder urlBuilder = new StringBuilder("http://api.odcloud.kr/api/15056091/v1/uddi:e6b792cd-5f5f-4c74-867c-83159645f0ec");
+            
+            // 필요한 파라미터 추가
             urlBuilder.append("?" + URLEncoder.encode("page", "UTF-8") + "=" + URLEncoder.encode("1", "UTF-8"));
             urlBuilder.append("&" + URLEncoder.encode("perPage", "UTF-8") + "=" + URLEncoder.encode("264", "UTF-8"));
             urlBuilder.append("&" + URLEncoder.encode("serviceKey", "UTF-8") + "=g%2BdzVqHbtyZ4yDYOaF3yYrZr0sZPNvlIWf2PAg2uvpPpjJav%2Fm%2B%2Bbyjs5mbKyj1W17CfFilBfaxHTpMupA6%2FxQ%3D%3D");
             urlBuilder.append("&" + URLEncoder.encode("type", "UTF-8") + "=" + URLEncoder.encode("json", "UTF-8"));
             urlBuilder.append("/" + URLEncoder.encode("20230301", "UTF-8"));
-            
+
+            // URL 객체 생성 및 연결 설정
             URL url = new URL(urlBuilder.toString());
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
             conn.setRequestMethod("GET");
             conn.setRequestProperty("Content-type", "application/json");
-           // System.out.println("Response code: " + conn.getResponseCode());
+            
+            // API 응답 처리
             BufferedReader rd;
-
             if (conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
                 rd = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
             } else {
@@ -43,14 +49,14 @@ public class BeachServiceImpl implements BeachService{
             rd.close();
             conn.disconnect();
 
-            // ������ Ÿ���� JSON���� ��ȯ
+            // JSON 응답을 BeachResultData 객체로 변환
             ObjectMapper objectMapper = new ObjectMapper();
             BeachResultData resultData = objectMapper.readValue(sb.toString(), BeachResultData.class);
 
             return resultData;
         } catch (Exception e) {
             e.printStackTrace();
-            return null; // ���� ó�� ����� ������ �Ŀ� �����ϰ� �������ּ���.
+            return null;
         }
     }
 }
